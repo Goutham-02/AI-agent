201ae7f3d89d4f34c69cf6d6c7ff1143a2005a7f
Goutham <gouthamshastry02@gmail.com>
2025-08-17 23:13:49 +0530

    Feat: Add ticket routes and controller for ticket management

---
+++ Backend/controllers/ticket.js
@@ -0,0 +1,72 @@
+import { inngest } from "../inngest/client.js";
+import Ticket from "../models/ticket.js";
+
+export const createTicket = async (req, res) => {
+    try {
+        const { title, description } = req.body;
+        if (!title || !description) {
+            return res.status(400).json({ error: "Title and description are required" });
+        }
+
+        const newTicket = await Ticket.create({
+            title,
+            description,
+            createdBy: req.user._id.toString()
+        });
+        await inngest.send({
+            name: "ticket/created",
+            data: {
+                ticketId: newTicket._id.toString(),
+                title,
+                description,
+                createdBy: req.user._id.toString()
+            }
+        });
+        res.status(201).json({
+            message: "Ticket created and processing started",
+            ticket: newTicket
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Ticket creation failed", details: error.message });
+    }
+};
+
+export const getTickets = async (req, res) => {
+    try {
+        const user = req.user;
+        const tickets = [];
+        if (user.role !== "user") {
+            tickets = Ticket.find({})
+                .populate("assignedTo", ["email", "_id"])
+                .sort({ createdAt: -1 });
+        } else {
+            tickets = await Ticket.find({ createdBy: user._id })
+                .select("title description status createdAt")
+                .sort({ createdAt: -1 });
+        }
+        res.status(200).json(tickets);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to retrieve tickets", details: error.message });
+    }
+};
+
+export const getTicket = async (req, res) => {
+    try {
+        const user = req.user;
+        let ticket;
+
+        if(user.role !== "user") {
+            ticket = await Ticket.findById(req.params.id)
+            .populate("assignedTo", ["email", "_id"]);
+        } else {
+            ticket = await Ticket.findOne({ _id: req.params.id, createdBy: user._id })
+            .select("title description status createdAt");
+        }
+        if (!ticket) {
+            return res.status(404).json({ error: "Ticket not found" });
+        }
+        res.status(200).json(ticket);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to retrieve ticket", details: error.message });
+    }
+};
--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { serve } from "inngest/express";
+import inngest from './inngest/client.js';
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-created.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,11 +15,17 @@ app.use(cors());
 app.use(express.json())
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use("/api/inngest", serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated]
+}));
 
 mongoose
     .connect(process.env.MONGO_URI)
-    .then(()=>{
+    .then(() => {
         console.log("MongoDB Connected");
-        app.listen(PORT, ()=>console.log("Server running at 3000"));
+        app.listen(PORT, () => console.log("Server running at 3000"));
     })
     .catch((e) => console.error(e));
